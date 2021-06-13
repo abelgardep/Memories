48ad8e44e372bb1a313430f9af836c950f77a225
Abel G <abelgardep@gmail.com>
2021-06-13 11:45:09 +0200

    Update theme colors

--- presentation/src/main/java/com/abelgardep/memories/ui/theme/Color.kt
+++ presentation/src/main/java/com/abelgardep/memories/ui/theme/Color.kt
@@ -2,7 +2,17 @@ package com.abelgardep.memories.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val BlueGray50 = Color(0xFFEEF0F2)
+val BlueGray200 = Color(0xFFADBBC4)
+val BlueGray300 = Color(0xFF8CA2AE)
+val BlueGray700 = Color(0xFF344955)
+val BlueGray800 = Color(0xFF232F34)
+
+val Orange300 = Color(0xFFfbd790)
+val Orange400 = Color(0xFFF9BE64)
+val Orange500 = Color(0xFFF9AA33)
+
+val Red200 = Color(0xFFCF7779)
+val Red400 = Color(0xFFef5350)
+
+val Black800 = Color(0xFF121212)
--- presentation/src/main/java/com/abelgardep/memories/ui/theme/Theme.kt
+++ presentation/src/main/java/com/abelgardep/memories/ui/theme/Theme.kt
@@ -5,26 +5,32 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = BlueGray200,
+    primaryVariant = BlueGray300,
+    onPrimary = Color.Black,
+    secondary = Orange300,
+    secondaryVariant = Orange300,
+    onSecondary = Color.Black,
+    error = Red200,
+    onError = Color.White,
+    background = Color.Black,
+    surface = Black800
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
+    primary = BlueGray700,
+    primaryVariant = BlueGray800,
     onPrimary = Color.White,
+    secondary = Orange500,
+    secondaryVariant = Orange400,
     onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    error = Red400,
+    onError = Color.White,
+    background = BlueGray50,
+    surface = Color.White,
 )
 
 @Composable
