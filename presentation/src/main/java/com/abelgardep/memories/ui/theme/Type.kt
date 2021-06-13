1d6157c838f7c83238f2d3ef9b5f4134f0471377
Abel G <abelgardep@gmail.com>
2021-06-13 12:47:33 +0200

    Add a custom font and update typography

--- presentation/src/main/java/com/abelgardep/memories/ui/theme/Type.kt
+++ presentation/src/main/java/com/abelgardep/memories/ui/theme/Type.kt
@@ -1,28 +1,86 @@
 package com.abelgardep.memories.ui.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
+import com.abelgardep.memories.R
+
+
+val WorkSans = FontFamily(
+    Font(R.font.worksans_light,  FontWeight.Light),
+    Font(R.font.worksans_regular,  FontWeight.Normal),
+    Font(R.font.worksans_medium,  FontWeight.Medium),
+    Font(R.font.worksans_semibold,  FontWeight.SemiBold),
+)
+
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+    h1 = TextStyle(
+        fontFamily = WorkSans,
+        fontSize = 96.sp,
+        fontWeight = FontWeight.Light,
+    ),
+    h2 = TextStyle(
+        fontFamily = WorkSans,
+        fontSize = 60.sp,
+        fontWeight = FontWeight.Light,
+    ),
+    h3 = TextStyle(
+        fontFamily = WorkSans,
+        fontSize = 48.sp,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
+    ),
+    h4 = TextStyle(
+        fontFamily = WorkSans,
+        fontSize = 30.sp,
+        fontWeight = FontWeight.SemiBold,
+    ),
+    h5 = TextStyle(
+        fontFamily = WorkSans,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.SemiBold,
+    ),
+    h6 = TextStyle(
+        fontFamily = WorkSans,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.SemiBold,
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = WorkSans,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.SemiBold,
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = WorkSans,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Bold,
+    ),
+    body1 = TextStyle(
+        fontFamily = WorkSans,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Normal,
+    ),
+    body2 = TextStyle(
+        fontFamily = WorkSans,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Medium,
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontFamily = WorkSans,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.SemiBold,
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontFamily = WorkSans,
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Bold,
+    ),
+    overline = TextStyle(
+        fontFamily = WorkSans,
+        fontSize = 12.sp,
+        fontWeight = FontWeight.SemiBold,
     )
-    */
 )
